a708585d7af1897ce35e367cc2c3da63e31a736d
knwanna <77506041+knwanna@users.noreply.github.com>
2025-08-17 01:55:23 +0100

    Create tailwind.config.js

---
+++ interface/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          600: '#7c3aed', // Primary brand color
+        },
+      }
+    }
+  }
+};
